ead3744da94d36f0604fa3448a5bab70e67cb824
pulgaresking <paulpulgares24@gmail.com>
2022-02-28 15:17:47 +0800

    restructure routes

--- routes/todo.js
+++ routes/todo.js
@@ -2,11 +2,11 @@ const express = require('express')
 const router = express.Router();
 const todo = require('../controllers/todo');
 const catchAsync = require('../utils/catchAsync');
-const {validateTodo, isLoggedIn} = require('../middleware');
+const { validateTodo, isLoggedIn } = require('../middleware');
 
-router.get('/', isLoggedIn, catchAsync(todo.index));
-
-router.post('/', isLoggedIn, validateTodo, catchAsync(todo.createTodo));
+router.route('/')
+    .get(isLoggedIn, catchAsync(todo.index))
+    .post(isLoggedIn, validateTodo, catchAsync(todo.createTodo));
 
 router.delete('/:id', isLoggedIn, catchAsync(todo.deleteTodo));
 
